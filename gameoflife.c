e091ba42246e076b6a69c0fea6b4fc9e8b18bd86
alexia <123456789+odin@users.noreply.github.com>
2025-04-13 13:06:31 +0300

    MODULE: Changed name         Changed the name from task1.c task1.h and task1_test.c to gameoflife.
    Signed-off-by: alexia <123456789+odin@users.noreply.github.com>

--- gameoflife.c
+++ gameoflife.c
@@ -1,5 +1,5 @@
 
-#include "task1.h"
+#include "gameoflife.h"
 
 void addAtBeginning(Nod** head, Data linie, Data coloana) {
     Nod *newNode = (Nod*)malloc(sizeof(Nod));
--- gameoflife_test.c
+++ gameoflife_test.c
@@ -1,5 +1,5 @@
 
-#include "task1.h"
+#include "gameoflife.h"
 
 int main(int argc, const char*  argv[])
 {  
