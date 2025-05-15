d82de30e9e7ebed2f837ab1f65fc63dd8ab657c9
alexia <123456789+odin@users.noreply.github.com>
2025-05-15 21:13:57 +0300

    MODULE: Task 2 using lists             I have decided to change task 2 completely in order to use only lists for efficiency.
    Signed-off-by: alexia <123456789+odin@users.noreply.github.com>

--- gameoflife.c
+++ gameoflife.c
@@ -1,85 +1,9 @@
+
 #include "gameoflife.h"
 
-void addAtBeginning(Nod ** head, Data linie, Data coloana) {
-    Nod * newNode = (Nod * ) malloc(sizeof(Nod));
-    newNode -> linie = linie;
-    newNode -> coloana = coloana;
-    newNode -> next = * head;
-    * head = newNode;
-}
-void addAtEnd(Nod ** head, Data linie, Data coloana) {
-    if ( * head == NULL) {
-        addAtBeginning( & * head, linie, coloana);
-        return;
-    }
-
-    Nod * aux = * head;
-
-    while (aux -> next != NULL)
-        aux = aux -> next;
-
-    Nod * newNode = (Nod * ) malloc(sizeof(Nod));
-    newNode -> linie = linie;
-    newNode -> coloana = coloana;
-    newNode -> next = NULL;
-    aux -> next = newNode;
-
-}
-
-void push(Node ** top, Nod * list) {
-    Node * newNode = (Node * ) malloc(sizeof(Node));
-    newNode -> list = list;
-    newNode -> next = * top;
-    * top = newNode;
-}
-
-void printStack(Node * top, FILE * fisier) {
-    if (top == NULL) {
-        return;
-    }
-    printStack(top -> next, fisier);
-    static int g = 1;
-
-    fprintf(fisier, "%d ", g++);
-
-    Nod * aux = top -> list;
-    while (aux) {
-        if (aux -> next != NULL) {
-            fprintf(fisier, "%d %d ", aux -> linie, aux -> coloana);
-        } else {
-            fprintf(fisier, "%d %d", aux -> linie, aux -> coloana);
-        }
-
-        aux = aux -> next;
-    }
-
-    fprintf(fisier, "\n");
-    top = top -> next;
-
-}
-
-void deleteStack(Node ** top) {
-    Node * temp;
-    while (( * top) != NULL) {
-        temp = * top;
-        * top = ( * top) -> next;
-        deletelist(temp -> list);
-        free(temp);
-    }
-}
-
-void deletelist(Nod * head) {
-    Nod * temp;
-    while (head != NULL) {
-        temp = head;
-        head = head -> next;
-        free(temp);
-    }
-
-}
-
-int nr_vecini_celula(char ** matrice, int N, int M, int i, int j) {
-    //test
+int nr_vecini_celula(char **matrice, int N, int M, int i, int j)
+{
+//test
     int k = 0;
 
     if (j + 1 < M && matrice[i][j + 1] == 'X')
@@ -102,236 +26,160 @@ int nr_vecini_celula(char ** matrice, int N, int M, int i, int j) {
     return k;
 }
 
-void reguli(char ** matrice, char ** matrice_noua, int N, int M) {
+void reguli(char **matrice, char **matrice_noua, int N, int M)
+{
     int i, j;
     int k;
 
-    for (i = 0; i < N; i++) {
-        for (j = 0; j < M; j++) {
+    for (i = 0; i < N; i++)
+    {
+        for (j = 0; j < M; j++)
+        {
             k = nr_vecini_celula(matrice, N, M, i, j);
 
-            if (matrice[i][j] == 'X' && (k < 2 || k > 3)) {
+            if (matrice[i][j] == 'X' && (k < 2 || k > 3))
                 matrice_noua[i][j] = '+';
 
-            } else if (matrice[i][j] == 'X' && (k == 2 || k == 3)) {
+            else if (matrice[i][j] == 'X' && (k == 2 || k == 3))
                 matrice_noua[i][j] = 'X';
 
-            } else if (matrice[i][j] == '+' && k == 3) {
+            else if (matrice[i][j] == '+' && k == 3)
                 matrice_noua[i][j] = 'X';
-
-            } else {
+            else
                 matrice_noua[i][j] = matrice[i][j];
-            }
         }
     }
+}
+void addAtBeginning(Nod** head, Data linie, Data coloana) {
+    Nod *newNode = (Nod*)malloc(sizeof(Nod));
+        newNode->linie = linie;
+        newNode->coloana = coloana;
+        newNode->next = *head;
+        *head = newNode;
+}
+void addAtEnd(Nod** head,  Data linie, Data coloana) {
+    if (*head == NULL) {
+    addAtBeginning(&*head, linie, coloana);
+    return; }
+
+
+    Nod *aux = *head;
+
+
+     while (aux->next != NULL)
+            aux = aux->next;
+
+    Nod *newNode = (Nod*)malloc(sizeof(Nod));
+    newNode->linie = linie;
+    newNode->coloana = coloana;
+    newNode->next = NULL;
+    aux->next = newNode;
+
 
 }
 
-void schimbari(char ** matrice, char ** matrice_noua, int N, int M, Node ** stack) {
-
-    Nod * lista = NULL;
-    int i, j;
-
-    for (i = 0; i < N; i++) {
-        for (j = 0; j < M; j++) {
-            if (matrice[i][j] != matrice_noua[i][j])
-
-            {
-                addAtEnd( & lista, i, j);
-
-            }
-        }
-    }
-    push(stack, lista);
-
-}
-
-Tree* insert(Nod* list) {
-
-    Tree* newNode = (Tree*)malloc(sizeof(Tree));
+void push(Node** top, Nod *list) {
+    Node* newNode = (Node*)malloc(sizeof(Node));
     newNode->list = list;
-    newNode->left = NULL;
-    newNode->right = NULL;
-    return newNode;
+    newNode->next = *top;
+    *top = newNode;
 }
 
-void reguli_B(char** matrice, char** matrice_noua, int N, int M) {
-    int i, j;
-    int k;
-
-   
-    for (i = 0; i < N; i++) {
-        for (j = 0; j < M; j++) {
-            matrice_noua[i][j] = matrice[i][j];
-        }
-    }
-
-    for (i = 0; i < N; i++) {
-        for (j = 0; j < M; j++) {
-            k = nr_vecini_celula(matrice, N, M, i, j);
-            
-            if (k == 2) {
-                matrice_noua[i][j] = 'X'; 
-            }
-        }
-    }
-}
-
-void copie_matrice(char** original, char** copie, int N, int M) {
-    for (int i = 0; i < N; i++) {
-        for (int j = 0; j < M; j++) {
-            copie[i][j] = original[i][j];
-        }
-    }
-}
-
-void arbore_creat(Tree* root, char** matrice, int N, int M, int level, int K) {
-
-    if (level >= K) {
+void printStack(Node* top, FILE* fisier) {
+    if (top == NULL) {
         return;
     }
-    
+    printStack(top->next, fisier);
+    static int g = 1;
 
-    char** matrice_B = (char**)malloc(N * sizeof(char*));
-    char** matrice_standard = (char**)malloc(N * sizeof(char*));
-    
-    for (int i = 0; i < N; i++) {
-        matrice_B[i] = (char*)malloc(M * sizeof(char));
-        matrice_standard[i] = (char*)malloc(M * sizeof(char));
+        fprintf(fisier, "%d ", g++);
+
+        Nod* aux = top->list;
+         while (aux) {
+            if(aux->next != NULL){
+                fprintf(fisier, "%d %d ", aux->linie, aux->coloana);}
+            else{
+                fprintf(fisier, "%d %d", aux->linie, aux->coloana);}
+
+            aux = aux->next;
+        }
+
+        fprintf(fisier,"\n");
+        top = top->next;
+
+}
+
+
+
+
+void deleteStack(Node** top) {
+    Node* temp;
+    while ((*top) != NULL) {
+        temp = *top;
+        *top = (*top)->next;
+        deletelist(temp->list);
+        free(temp);
     }
-    
-    
-    char** matrice_B_noua = (char**)malloc(N * sizeof(char*));
-    char** matrice_standard_noua = (char**)malloc(N * sizeof(char*));
-    
-    for (int i = 0; i < N; i++) {
-        matrice_B_noua[i] = (char*)malloc(M * sizeof(char));
-        matrice_standard_noua[i] = (char*)malloc(M * sizeof(char));
+}
+
+void deletelist(Nod* head){
+    Nod* temp;
+    while (head != NULL) {
+        temp = head;
+        head = head->next;
+        free (temp);
     }
-    
+
+}
+
+int celule_vii(Nod* lista, int l, int c) {
+    while (lista) {
+        if (lista->linie == l && lista->coloana == c)
+            return 1;
+        lista = lista->next;
+    }
+    return 0;
+}
+
+Nod* copie_lista(Nod* original) {
+    Nod* copie = NULL;
+    Nod* aux = original;
+    while (aux) {
+        addAtEnd(&copie, aux->linie, aux->coloana);
+        aux = aux->next;
+    }
+    return copie;
+}
+
+Nod* generatii( int N, int M,  Nod* lista_intermediara){
   
-    copie_matrice(matrice, matrice_B, N, M);
-    copie_matrice(matrice, matrice_standard, N, M);
-    
-   
-    reguli_B(matrice_B, matrice_B_noua, N, M);
-    reguli(matrice_standard, matrice_standard_noua, N, M);
-    
-   
-    Nod* lista_B = NULL;
-    Nod* lista_standard = NULL;
-    
-   
-    for (int i = 0; i < N; i++) {
-        for (int j = 0; j < M; j++) {
-            if (matrice_B[i][j] != matrice_B_noua[i][j]) {
-                addAtEnd(&lista_B, i, j);
-            }
-        }
-    }
-    
-  
-    for (int i = 0; i < N; i++) {
-        for (int j = 0; j < M; j++) {
-            if (matrice_standard[i][j] != matrice_standard_noua[i][j]) {
-                addAtEnd(&lista_standard, i, j);
-            }
-        }
-    }
-    
-    
-    root->left = insert(lista_B);
-    root->right = insert(lista_standard);
-    
-    
-    arbore_creat(root->left, matrice_B_noua, N, M, level + 1, K);
-    arbore_creat(root->right, matrice_standard_noua, N, M, level + 1, K);
+   Nod *lista_noua = NULL;
 
-    for (int i = 0; i < N; i++) {
-        free(matrice_B[i]);
-        free(matrice_standard[i]);
-        free(matrice_B_noua[i]);
-        free(matrice_standard_noua[i]);
+    int i = 0, j = 0, vecini = 0;
+    for ( i = 0; i < N; i++) {
+        for ( j = 0; j < M; j++) {
+            vecini = 0;
+    
+            if (i > 0 && j > 0 && celule_vii(lista_intermediara, i - 1, j - 1)) vecini++;
+            if (i > 0 && celule_vii(lista_intermediara, i - 1, j)) vecini++;
+            if (i > 0 && j < M - 1 && celule_vii(lista_intermediara, i - 1, j + 1)) vecini++;
+    
+            if (j > 0 && celule_vii(lista_intermediara, i, j - 1)) vecini++;
+            if (j < M - 1 && celule_vii(lista_intermediara, i, j + 1)) vecini++;
+    
+            if (i < N - 1 && j > 0 && celule_vii(lista_intermediara, i + 1, j - 1)) vecini++;
+            if (i < N - 1 && celule_vii(lista_intermediara, i + 1, j)) vecini++;
+            if (i < N - 1 && j < M - 1 && celule_vii(lista_intermediara, i + 1, j + 1)) vecini++;
+    
+            int eVie = celule_vii(lista_intermediara, i, j);
+    
+            if ( (!eVie && vecini == 3) || (eVie && (vecini < 2)) ||  (eVie && (vecini > 3 ))) {
+                addAtEnd(&lista_noua, i, j); 
+            }                           
+        }                              
     }
-    free(matrice_B);
-    free(matrice_standard);
-    free(matrice_B_noua);
-    free(matrice_standard_noua);
+
+
+   return lista_noua;
 }
 
-
-
-void preordine(Tree* root, char** matrice, int N, int M, FILE* fisier) {
-    if (root == NULL) {
-        return;
-    }
-    
-   
-    for (int i = 0; i < N; i++) {
-        for (int j = 0; j < M; j++) {
-            fprintf(fisier, "%c", matrice[i][j]);
-        }
-        fprintf(fisier, "\n");
-    }
-    fprintf(fisier, "\n");
-    
-
-    char** matrice_left = (char**)malloc(N * sizeof(char*));
-    char** matrice_right = (char**)malloc(N * sizeof(char*));
-    
-    for (int i = 0; i < N; i++) {
-        matrice_left[i] = (char*)malloc(M * sizeof(char));
-        matrice_right[i] = (char*)malloc(M * sizeof(char));
-        for (int j = 0; j < M; j++) {
-            matrice_left[i][j] = matrice[i][j];
-            matrice_right[i][j] = matrice[i][j];
-        }
-    }
-    
-   
-    if (root->left) {
-        Nod* current = root->left->list;
-        while (current != NULL) {
-            if (matrice_left[current->linie][current->coloana] == 'X') {
-                matrice_left[current->linie][current->coloana] = '+';
-            } else {
-                matrice_left[current->linie][current->coloana] = 'X';
-            }
-            current = current->next;
-        }
-        preordine(root->left, matrice_left, N, M, fisier);
-    }
-    
-    
-    if (root->right) {
-        Nod* current = root->right->list;
-        while (current != NULL) {
-            if (matrice_right[current->linie][current->coloana] == 'X') {
-                matrice_right[current->linie][current->coloana] = '+';
-            } else {
-                matrice_right[current->linie][current->coloana] = 'X';
-            }
-            current = current->next;
-        }
-        preordine(root->right, matrice_right, N, M, fisier);
-    }
-   
-    for (int i = 0; i < N; i++) {
-        free(matrice_left[i]);
-        free(matrice_right[i]);
-    }
-    free(matrice_left);
-    free(matrice_right);
-}
-
-void deleteTree(Tree* root) {
-    if (root == NULL) {
-        return;
-    }
-    
- 
-    deleteTree(root->left);
-    deleteTree(root->right);
-    deletelist(root->list);
-    free(root);
-}
--- gameoflife.h
+++ gameoflife.h
@@ -6,31 +6,25 @@ typedef struct Nod {
     Data linie;
     Data coloana;
     struct Nod* next;
-} Nod;
+
+}Nod;
 
 typedef struct Node {
     Nod* list;
     struct Node* next;
-} Node;
 
-typedef struct Tree {
-    Nod* list;
-    struct Tree* left;
-    struct Tree* right;
-} Tree;
+}Node;
 
-int nr_vecini_celula(char** matrice, int N, int M, int i, int j);
-void reguli(char** matrice, char** matrice_noua, int N, int M);
-void schimbari(char** matrice, char** matrice_noua, int N, int M, Node** stack);
+
+
+int nr_vecini_celula(char **matrice, int N, int M, int i, int j);
+void reguli(char **matrice, char **matrice_noua, int N, int M);
+Nod* generatii( int N, int M,  Nod* lista_intermediara);
+int celule_vii(Nod* lista, int l, int c);
 void addAtBeginning(Nod** head, Data linie, Data coloana);
-void addAtEnd(Nod** head, Data linie, Data coloana);
+void addAtEnd(Nod** head,  Data linie, Data coloana);
 void push(Node** top, Nod* list);
 void printStack(Node* top, FILE* fisier);
 void deletelist(Nod* head);
 void deleteStack(Node** top);
-void reguli_B(char** matrice, char** matrice_noua, int N, int M);
-Tree* insert(Nod* list);
-void arbore_creat(Tree* root, char** matrice, int N, int M, int level, int K);
-void preordine(Tree* root, char** matrice, int N, int M, FILE* fisier);
-void deleteTree(Tree* root);
-void copie_matrice(char** original, char** copie, int N, int M);
+Nod* copie_lista(Nod* original);
